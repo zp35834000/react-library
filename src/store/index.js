e8cb56184a517dd773c1028b0bf22711e552d1b0
zp <zhao_4365637@163.com>
2018-10-25 16:42:36 +0800

    增加了redux thunk、logger中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {createLogger} from 'redux-logger'
+
 import reducer from '../redux/reducers'
 
+const loggerMiddleware = createLogger();
+
 export default function configureStore(init) {
-    const store = createStore(reducer, init);
+    const store = createStore(
+        reducer, 
+        init,
+        applyMiddleware(
+            thunkMiddleware,
+            loggerMiddleware
+        ));
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
         module.hot.accept('../redux/reducers', () => {
