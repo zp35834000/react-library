8258f76714f58be7620bcaf9c16fa0b0ebca5338
zp <zhao_4365637@163.com>
2018-11-06 16:54:47 +0800

    测试了树形表格，menu.js，下一步会开发初步权限界面menu

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Home from '../pages/test/home'
 import Story from '../pages/test/story'
 import Statistics from '../pages/statistics'
 import AntTest from '../pages/antTest'
-
+import Menu from '../pages/authority/menu'
 
 const route = () => (
     <BrowserRouter>
@@ -15,6 +15,7 @@ const route = () => (
             <Route path='/story' component={Story}></Route>
             <Route path='/statistics' component={Statistics}></Route>
             <Route path='/antTest' component={AntTest}></Route>
+            <Route path='/menu' component={Menu}></Route>
         </div>
     </BrowserRouter>
 )
