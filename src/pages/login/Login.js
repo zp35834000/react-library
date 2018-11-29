49dd253ecfb5ca91db3098f1827306ef012db4e8
zp <zhao_4365637@163.com>
2018-11-29 21:28:42 +0800

    开发了登录界面

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,60 @@
 import React from 'react'
 
-class Login extends React.Component{
-    render(){
-        return (
-            <div>登录界面</div>
-        )
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import './login.css'
+import {CanvasParticle} from './canvas-particle'
+const FormItem = Form.Item;
+
+
+class NormalLoginForm extends React.Component {
+
+    componentWillMount(){
+        var config = {
+            vx : 4,
+            vy : 4,
+            height : 2,
+            width : 2,
+            count : 100,
+            color : "121, 162, 185",
+            stroke : "100, 200, 180",
+            dist : 6000,
+            e_dist : 20000,
+            max_conn : 10
+        }
+        CanvasParticle(config);
     }
+
+  render() {
+    return (
+        <div class="page">
+            <div class="loginwarrp">
+                <div class="logo">图书管理系统</div>
+                <div class="login_form">
+                    <form id="Login" name="Login" method="post" onsubmit="" action="">
+                        <li class="login-item">
+                            <span>用户名：</span>
+                            <input type="text" id="username" name="UserName" class="login_input" />
+                                                <span id="count-msg" class="error"></span>
+                        </li>
+                        <li class="login-item">
+                            <span>密　码：</span>
+                            <input type="password" id="password" name="password" class="login_input" />
+                                                <span id="password-msg" class="error"></span>
+                        </li>
+                        <li class="login-sub">
+                            <input type="submit" name="Submit" value="登录" />
+                                                <input type="reset" name="Reset" value="重置" />
+                        </li>                      
+                </form>
+                </div>
+            </div>
+        </div>
+    );
+  }
 }
 
-export default Login;
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+
+
+export default WrappedNormalLoginForm;
